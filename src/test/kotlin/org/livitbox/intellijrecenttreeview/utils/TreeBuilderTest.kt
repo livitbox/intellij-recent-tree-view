b313a23a3ee69a0fdbb819f430df6f5dfb4a73e2
gjorgievskivlatko <gjorgievskivlatko@gmail.com>
2025-11-03 09:27:54 +0100

    Fixed failing tests

--- src/test/kotlin/org/livitbox/intellijrecenttreeview/utils/TreeBuilderTest.kt
+++ src/test/kotlin/org/livitbox/intellijrecenttreeview/utils/TreeBuilderTest.kt
@@ -4,6 +4,7 @@ import com.intellij.openapi.Disposable
 import com.intellij.openapi.actionSystem.ActionManager
 import com.intellij.openapi.application.Application
 import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.project.Project
 import kotlinx.collections.immutable.persistentListOf
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
@@ -18,6 +19,7 @@ import java.nio.file.Paths
 class TreeBuilderTest() {
 
     lateinit var treeBuilder: TreeBuilder
+    lateinit var project: Project
 
     companion object {
         @BeforeAll
@@ -37,6 +39,7 @@ class TreeBuilderTest() {
     fun setupUp() {
         val mockSettings = mock<RecentProjectsTreeViewSettingsState>()
         this.treeBuilder = TreeBuilder(mockSettings)
+        this.project = mock()
     }
 
     @Test
@@ -47,10 +50,10 @@ class TreeBuilderTest() {
             Paths.get("/user3/test3")
         )
 
-        val result = treeBuilder.buildTree(paths)
+        val result = treeBuilder.buildTree(project, paths)
 
         assertNotNull(result)
-        assertEquals(3, result.size)
+        assertEquals(5, result.size)
     }
 
     @Test
@@ -61,9 +64,9 @@ class TreeBuilderTest() {
             Paths.get("/user2/test3")
         )
 
-        val result = treeBuilder.buildTree(paths)
+        val result = treeBuilder.buildTree(project, paths)
 
         assertNotNull(result)
-        assertEquals(2, result.size)
+        assertEquals(4, result.size)
     }
 }
